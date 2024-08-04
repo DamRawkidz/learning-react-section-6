cf3976ae831f2a13157638f991a32bb268dc3af0
Phet <rawkidz@hotmail.com>
2024-08-05 02:00:09 +0700

    [update] - add sider bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import ProjectSideBar from "./components/ProjectSidebar";
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <ProjectSideBar></ProjectSideBar>
     </>
   );
 }
