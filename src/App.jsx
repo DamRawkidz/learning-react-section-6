1c8f66acf5fe7f10ad285ef985976a5320414559
Phet <rawkidz@hotmail.com>
2024-08-06 02:00:08 +0700

    [update] - add page to add prohect and display some data in form and validate aalso

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,53 @@
+import { useState } from "react";
+import NewProject from "./components/newProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSideBar from "./components/ProjectSidebar";
 function App() {
+
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+
+    })
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState(prevState => {
+      const projectId = Math.random()
+      const newProject = {
+        ...projectData,
+        id: projectId
+      }
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...projectState.projects, newProject]
+      }
+    })
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <>
-      <ProjectSideBar></ProjectSideBar>
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectSideBar projects={projectState.projects} onStartAddProject={handleStartAddProject} />
+        {content}
+      </main>
     </>
   );
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+import { forwardRef } from "react"
+const input = forwardRef(function Input({ textarea, label, ...props }, ref) {
+    const classname = "w-full p-1 border-b-2  rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+    return <p className="flex flex-col gap-1 my-4">
+        <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+        {textarea ? <textarea ref={ref} className={classname} {...props} /> : <input ref={ref} className={classname} {...props} />}
+    </p>
+})
+
+export default input
