4b817016629ef266cfebabad963c5c2ee61775c5
Phet <rawkidz@hotmail.com>
2024-08-08 02:06:04 +0700

    [update] - styling modal and add page selected project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NewProject from "./components/newProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSideBar from "./components/ProjectSidebar";
+import SelectedProject from "./components/SelectedProject";
 function App() {
 
   const [projectState, setProjectState] = useState({
@@ -15,7 +16,25 @@ function App() {
         ...prevState,
         selectedProjectId: null
       }
+    })
+  }
 
+  function handleSelectedProject(id) {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id
+      }
+    })
+  }
+
+
+  function handleCancelAddPrpject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined
+      }
     })
   }
 
@@ -34,10 +53,12 @@ function App() {
     })
   }
 
-  let content;
+  let selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId)
+
+  let content = <SelectedProject project={selectedProject} />
 
   if (projectState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddPrpject} />
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
   }
@@ -45,7 +66,11 @@ function App() {
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <ProjectSideBar projects={projectState.projects} onStartAddProject={handleStartAddProject} />
+        <ProjectSideBar
+          projects={projectState.projects}
+          onStartAddProject={handleStartAddProject}
+          onSelectProject={handleSelectedProject}
+        />
         {content}
       </main>
     </>
